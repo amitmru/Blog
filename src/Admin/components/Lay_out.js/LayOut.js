c5460732424c3a3b2d9d93f37420b9a238cf9306
srushtichangela267@gmail.com <srushtichangela267@gmail.com>
2023-11-02 17:46:51 +0530

    Done

--- src/Admin/components/Lay_out.js/LayOut.js
+++ src/Admin/components/Lay_out.js/LayOut.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
@@ -13,23 +13,15 @@ import Box from '@mui/material/Box';
 
 function Lay_out({ children }) {
 
-    // const iconItems = [
-    //     { label: "Dahsboard", icon: <HomeOutlinedIcon />, to: '/admin/dahsboard' },
-    //     { label: "Like", icon: <ThumbUpIcon />, to: '/admin/like' },
-    //     { label: "Content", icon: <FolderCopyOutlinedIcon />, to: '/admin/content' }
-    // ]
+    let [toggle, setToogle] = useState(true);
 
-    const handleToggleSwitch = (name) => {
-        console.log(name);
-    }
-
-    const handleSearch = () => {
-        console.log('search');
+    const handleToggleSwitch = () => {
+        setToogle(!toggle)
     }
 
     return (
         <div>
-            <nav>
+            <nav className={toggle ? 'sidebar' : 'close'}>
                 <div className="logo-name">
                     <div className="logo-image">
                         <img src="assets/Images/logo.png" alt />
@@ -37,37 +29,8 @@ function Lay_out({ children }) {
                     <span className="logo_name">CodingLab</span>
                 </div>
                 <div className="menu-items">
-                    {/* <List>
-                        {iconItems.map((value, index) => (
-                            <ListItem
-                                key={index}
-                                component={Link}
-                                to={value.to}
-                            >
-                                <ListItemButton
-                                    sx={{
-                                        minHeight: 48,
-                                        px: 2.5,
-                                    }}
-                                >
-                                    <ListItemIcon
-                                        sx={{
-                                            minWidth: 0,
-                                            justifyContent: 'center',
-                                        }}
-                                    >
-                                        {value.icon}
-                                    </ListItemIcon>
-                                    <ListItemText primary={value.label} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List> */}
-                    {/* <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                        {children}
-                    </Box> */}
                     <ul className="nav-links">
-                        <li><NavLink to="admin/dahsboard">
+                        <li><NavLink to="/dahsboard">
                             <i className="uil uil-estate" />
                             <span className="link-name">Dahsboard</span>
                         </NavLink></li>
@@ -111,97 +74,15 @@ function Lay_out({ children }) {
             </nav >
             <section className="dashboard">
                 <div className="top">
-                    <i className="uil uil-bars sidebar-toggle" onClick={handleToggleSwitch('toggle')}/>
+                    <i className="uil uil-bars sidebar-toggle" onClick={() => handleToggleSwitch()}/>
                     <div className="search-box">
                         <i className="uil uil-search" />
-                        <input type="text" placeholder="Search here..." onChange={handleSearch()}/>
+                        <input type="text" placeholder="Search here..." />
                     </div>
                     <img src="assets/Images/profile.jpg" alt />
                 </div>
-                <div className="dash-content">
-                    <div className="overview">
-                        <div className="title">
-                            <i className="uil uil-tachometer-fast-alt" />
-                            <span className="text">Dashboard</span>
-                        </div>
-                        <div className="boxes">
-                            <div className="box box1">
-                                <i className="uil uil-thumbs-up" />
-                                <span className="text">Total Likes</span>
-                                <span className="number">50,120</span>
-                            </div>
-                            <div className="box box2">
-                                <i className="uil uil-comments" />
-                                <span className="text">Comments</span>
-                                <span className="number">20,120</span>
-                            </div>
-                            <div className="box box3">
-                                <i className="uil uil-share" />
-                                <span className="text">Total Share</span>
-                                <span className="number">10,120</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="activity">
-                        <div className="title">
-                            <i className="uil uil-clock-three" />
-                            <span className="text">Recent Activity</span>
-                        </div>
-                        <div className="activity-data">
-                            <div className="data names">
-                                <span className="data-title">Name</span>
-                                <span className="data-list">Prem Shahi</span>
-                                <span className="data-list">Deepa Chand</span>
-                                <span className="data-list">Manisha Chand</span>
-                                <span className="data-list">Pratima Shahi</span>
-                                <span className="data-list">Man Shahi</span>
-                                <span className="data-list">Ganesh Chand</span>
-                                <span className="data-list">Bikash Chand</span>
-                            </div>
-                            <div className="data email">
-                                <span className="data-title">Email</span>
-                                <span className="data-list">premshahi@gmail.com</span>
-                                <span className="data-list">deepachand@gmail.com</span>
-                                <span className="data-list">prakashhai@gmail.com</span>
-                                <span className="data-list">manishachand@gmail.com</span>
-                                <span className="data-list">pratimashhai@gmail.com</span>
-                                <span className="data-list">manshahi@gmail.com</span>
-                                <span className="data-list">ganeshchand@gmail.com</span>
-                            </div>
-                            <div className="data joined">
-                                <span className="data-title">Joined</span>
-                                <span className="data-list">2022-02-12</span>
-                                <span className="data-list">2022-02-12</span>
-                                <span className="data-list">2022-02-13</span>
-                                <span className="data-list">2022-02-13</span>
-                                <span className="data-list">2022-02-14</span>
-                                <span className="data-list">2022-02-14</span>
-                                <span className="data-list">2022-02-15</span>
-                            </div>
-                            <div className="data type">
-                                <span className="data-title">Type</span>
-                                <span className="data-list">New</span>
-                                <span className="data-list">Member</span>
-                                <span className="data-list">Member</span>
-                                <span className="data-list">New</span>
-                                <span className="data-list">Member</span>
-                                <span className="data-list">New</span>
-                                <span className="data-list">Member</span>
-                            </div>
-                            <div className="data status">
-                                <span className="data-title">Status</span>
-                                <span className="data-list">Liked</span>
-                                <span className="data-list">Liked</span>
-                                <span className="data-list">Liked</span>
-                                <span className="data-list">Liked</span>
-                                <span className="data-list">Liked</span>
-                                <span className="data-list">Liked</span>
-                                <span className="data-list">Liked</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </section>
+
         </div >
 
     );
--- src/Routes/AdminRoute.js
+++ src/Routes/AdminRoute.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import LayOut from '../Admin/components/Lay_out.js/LayOut';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from '../Admin/conatiner/Dashboard/Dashboard';
 
 function AdminRoute(props) {
-    return (
-        <div>
-          <LayOut>
-
-          </LayOut>
-        </div>
-    );
+  return (
+    <div>
+      <LayOut>
+        <Routes>
+          <Route path='/dahsboard' element={<Dashboard />} />
+        </Routes>
+      </LayOut>
+    </div>
+  );
 }
 
 export default AdminRoute;
